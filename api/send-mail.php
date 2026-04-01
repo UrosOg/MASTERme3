97e73b84f954b9a02bc3f39819f5826d2669ead0
Uros Ognjanovic <56482344+UrosOg@users.noreply.github.com>
2026-04-01 20:59:29 +0200

    Update send-mail.php
    new email adress final

--- api/send-mail.php
+++ api/send-mail.php
@@ -19,7 +19,7 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Username = 'masterme26122025@gmail.com';
-$mail->Password = 'iiln jfds abqq orey';
+$mail->Password = 'lawp ehqk kwfc rijp';
 $mail->SMTPSecure = 'tls';
 $mail->Port = 587;
 
